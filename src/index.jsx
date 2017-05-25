093362cc161a187809bde9b98d168125c8881317
KINAN WHYTE <kinanwhyte@gmail.com>
2017-05-25 16:22:49 -0600

    Create webpack index.jsx and index.scss entry points

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/app/App.jsx'
+
+import './index.scss'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
